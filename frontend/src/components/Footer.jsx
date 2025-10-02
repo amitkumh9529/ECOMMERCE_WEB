6e7467ef7c500c374e8dcda35df9d8ed82836630
amitkumh9529 <amith3169@gmail.com>
2025-10-03 01:30:59 +0530

    new

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga totam ratione explicabo suscipit, nemo asperiores.</p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+91-9876543210</li>
+                <li>contact@zohomail.com</li>
+                
+            </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
